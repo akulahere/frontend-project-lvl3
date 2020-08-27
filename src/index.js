4676d3fb08237884f7c5c6ccc7c70b1bcb0e07bb
dmitry <akulinind@gmail.com>
2020-08-27 11:29:46 +0300

    redeploy

--- src/index.js
+++ src/index.js
@@ -3,3 +3,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './component.js';
 console.log("Hello World from your main file!");
+
+
+function getTask(weekday) {
+  if (weekday === 'monday') {
+    return 'Write a new module for the program';
+  } else if (weekday === 'tuesday') {
+    return 'Test the module and find bugs';
+  } else if (weekday === 'wednesday') {
+    return 'Write a new module for the program';
+  } else if (weekday === 'thursday') {
+    return 'Test the module and find bugs';
+  } else if (weekday === 'friday') {
+    return 'You need to meet the boss today';
+  } else if (weekday === 'saturday') {
+    return 'Spend the evening with your friends';
+  } else if (weekday === 'sunday') {
+    return 'Go to the movies in the evening';
+  } else {
+    return 'Entered the wrong day of the week';
+  }
+  const a = weekday => {
+    switch (weekday) {
+      case 'monday':
+        console.log();
+        break;
+      case 'monday':
+        console.log();
+        break;
+      case 'monday':
+        console.log();
+        break;
+      case 'monday':
+        console.log();
+        break;
+      case 'monday':
+        console.log();
+        break;
+      case 'monday':
+        console.log();
+        break;
+      default:
+        console.log();
+
+
+
+
+    }
+  };
+}
